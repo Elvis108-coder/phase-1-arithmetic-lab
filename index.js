0a74321e9d15c983d09c6c221863c154b9ff5c5c
Elviskiprono <biggeelvo831@gmail.com>
2025-04-03 14:21:13 +0300

    Arithmetic lab

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-
+let num1 =31;
+let num2 = 2;
+let multiply =num1* num2;
+let random = Math.floor(Math.random() * 100) + 1;
+let num3 = 28;
+let num4 = 6;
+let mod = num3 % num4;
+let max = Math.max(12,20,14,5);
 // Write your code here
